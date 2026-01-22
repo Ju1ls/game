bb06ba0dd967e46f37b19349955d04cbcbc27b6e
jull <sefljulie@gmail.com>
2026-01-22 20:10:05 +0100

    feat: added movement

--- src/main/java/cz/jull/command/type/EnterPlaceCommand.java
+++ src/main/java/cz/jull/command/type/EnterPlaceCommand.java
@@ -1,8 +1,14 @@
 package cz.jull.command.type;
 
+import cz.jull.Game;
+import cz.jull.Player;
 import cz.jull.command.Command;
 import cz.jull.command.PostCommandActionType;
+import cz.jull.models.locations.Location;
+import cz.jull.models.locations.Side;
 import lombok.Getter;
+
+import java.util.Map;
 
 public class EnterPlaceCommand extends Command {
     @Getter
@@ -10,7 +16,59 @@ public class EnterPlaceCommand extends Command {
 
 
     @Override
-    public PostCommandActionType execute(String[] args) {
+    public PostCommandActionType execute(String[] args, Game game) {
+        Player player = game.getPlayer();
+        Side currentSide = player.getCurrentSide();
+        Location currentLoc = player.getCurrentLocation();
+        Location nextLocation = currentSide.getNeighbor();
+        String currentDirection = null;
+
+        if (nextLocation == null) {
+            System.out.println("There is no way to go in this direction.");
+            return PostCommandActionType.NONE;
+        }
+
+        if (nextLocation.isLocked()) {
+            System.out.println("The " + nextLocation.getName() + " is currently locked.");
+            return PostCommandActionType.NONE;
+        }
+
+        for (Map.Entry<String, Side> entry : currentLoc.getSides().entrySet()) {
+            if (entry.getValue() == currentSide) {
+                currentDirection = entry.getKey();
+                break;
+            }
+        }
+
+        if (currentDirection == null) {
+            System.out.println("Error: Could not determine current direction.");
+            return PostCommandActionType.NONE;
+        }
+
+        String arrivalDirection = getOppositeDirection(currentDirection);
+        Side arrivalSide = nextLocation.getSides().get(arrivalDirection);
+
+        if (arrivalSide == null) {
+            System.out.println("Error: The path seems blocked on the other side.");
+            return PostCommandActionType.NONE;
+        }
+
+        player.setCurrentLocation(nextLocation);
+        player.setCurrentSide(arrivalSide);
+
+        System.out.println("You have entered: " + nextLocation.getName());
+        System.out.println("You are now at the " + arrivalDirection + " side.");
+
         return PostCommandActionType.NONE;
+    }
+
+    private String getOppositeDirection(String direction) {
+        return switch (direction.toLowerCase()) {
+            case "north" -> "south";
+            case "south" -> "north";
+            case "east" -> "west";
+            case "west" -> "east";
+            default -> throw new IllegalStateException("Unexpected value: " + direction.toLowerCase());
+        };
     }
 }
--- src/main/java/cz/jull/command/type/GoCommand.java
+++ src/main/java/cz/jull/command/type/GoCommand.java
@@ -1,7 +1,11 @@
 package cz.jull.command.type;
 
+import cz.jull.Game;
 import cz.jull.command.Command;
 import cz.jull.command.PostCommandActionType;
+
+import cz.jull.models.locations.Location;
+import cz.jull.models.locations.Side;
 import lombok.Getter;
 
 public class GoCommand extends Command {
@@ -10,7 +14,31 @@ public class GoCommand extends Command {
     private final String name = "go";
 
     @Override
-    public PostCommandActionType execute(String[] args) {
+    public PostCommandActionType execute(String[] args, Game game) {
+        String direction = args[0].toLowerCase();
+        Location currentLoc = game.getPlayer().getCurrentLocation();
+
+        switch (direction) {
+            case "north" -> {
+                Side northSide = currentLoc.getSides().get("north");
+                game.getPlayer().setCurrentSide(northSide);
+            }
+            case "south" -> {
+                Side southSide = currentLoc.getSides().get("south");
+                game.getPlayer().setCurrentSide(southSide);
+            }
+            case "east" -> {
+                Side eastSide = currentLoc.getSides().get("east");
+                game.getPlayer().setCurrentSide(eastSide);
+            }
+            case "west" -> {
+                Side westSide = currentLoc.getSides().get("west");
+                game.getPlayer().setCurrentSide(westSide);
+            }
+            default -> {
+                System.out.println("There is no side like that."); //TODO pozdeji smazu System.out.println(); a vymenim za neco jineho
+            }
+        }
         return PostCommandActionType.NONE;
     }
 }

08be74d6d370b272de59fb83d687c67ead332a35
jull <sefljulie@gmail.com>
2026-02-25 18:34:54 +0100

    refactor: move initial dialog to constant

--- src/main/java/cz/jull/Game.java
+++ src/main/java/cz/jull/Game.java
@@ -7,12 +7,10 @@ import cz.jull.mechanics.FightManager;
 import cz.jull.mechanics.MentalHealthManager;
 import cz.jull.mechanics.ScheduledTaskManager;
 import cz.jull.mechanics.dialog.DialogManager;
-import cz.jull.models.Item;
 import cz.jull.models.locations.Direction;
 import lombok.Getter;
 
 import java.io.IOException;
-import java.util.List;
 import java.util.Scanner;
 
 public class Game {
@@ -39,13 +37,18 @@ public class Game {
 
     private boolean running = true;
 
+    private static final String INITIAL_DIALOG = """
+                You wake up in an abandoned store, feeling extremely confused and with no memory of what happened or how you got there.\s
+                As Aris, you must now explore the ruined city to find your way to safety.\s
+                \nType your first command (type 'help' for more commands):\s""";
+
     public void startGame() throws IOException, InterruptedException {
         loadGame();
         scheduledTaskManager.startAll();
         player.setCurrentLocation(gameData.getLocations().getFirst());
         player.setCurrentSide(player.getCurrentLocation().getSides().get(Direction.NORTH));
         commandManager.initialization();
-        System.out.println(initialDialog());
+        System.out.println(INITIAL_DIALOG);
 
         while (running) {
             System.out.print(">>");
@@ -62,11 +65,4 @@ public class Game {
         scheduledTaskManager.register(new DetectorBatteryManager(this));
     }
 
-    private String initialDialog() {
-        return """
-                You wake up in an abandoned store, feeling extremely confused and with no memory of what happened or how you got there.\s
-                As Aris, you must now explore the ruined city to find your way to safety.\s
-                \nType your first command (type 'help' for more commands):\s""";
-
-    }
 }
